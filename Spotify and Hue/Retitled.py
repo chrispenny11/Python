20c5f7aa613e26f6b480820fdaa4600d3f176cfd
Chris Penny <chrispenny11@gmail.com>
2018-11-01 19:04:21 -0400

    Recommented and removed some debugging code

--- Spotify and Hue/Retitled.py
+++ Spotify and Hue/Retitled.py
@@ -1,64 +1,46 @@
-
+# Initialize
 import os
 import sys
 import json
 import spotipy
 import spotipy.util as util
-from json.decoder import JSONDecodeError
+from json.decoder import JSONDecodeError as JSONDE
 import urllib.request as req
 import numpy as np
 
+# Define function for obtaining colors to pass to Philips Hue API Wrapper (phue package)
 def get_artwork_colors():
-    # Get the username from terminal
-    #username = sys.argv[1]
+    
+    # Define scope
     scope = 'current_user_playing_track user-read-private user-read-playback-state user-modify-playback-state'
 
-    # Erase cache and prompt for user permission
+    # Obtain Spotify Token (username, client_id, and client_secret will vary per user - for additional information see Spotify API documentation.)
     try:
         token = util.prompt_for_user_token("Chris Penny", client_id='dd41386aabca41aa8dd7ba2f947782b3',client_secret='4bf7eefcbf7241298d92b9f5ad6fe3f0',redirect_uri='http://google.com/')
-    #    token = util.prompt_for_user_token(username, scope) # add scope
-    except (AttributeError, JSONDecodeError):
+    # Error Case - Remove token cache and replace
+    except (AttributeError, JSONDE):
         os.remove(f".cache-{username}")
-    #    token = util.prompt_for_user_token(username, scope) # add scope
         token = util.prompt_for_user_token("Chris Penny", client_id='dd41386aabca41aa8dd7ba2f947782b3',client_secret='4bf7eefcbf7241298d92b9f5ad6fe3f0',redirect_uri='http://google.com/')
 
-    # Create our spotify object with permissions
-    sp = spotipy.Spotify(auth=token)
+    # Generate spotipy object using Spotify Token
+    spot_obj = spotipy.Spotify(auth=token)
 
-    # Get current device
-    #devices = spotifyObject.devices()
-    #deviceID = devices['devices'][0]['id']
+    # Call spotipy attribute for the currently playing track
+    track = spot_obj.current_user_playing_track()
+    #print(list(track)) # For Debug
 
-    ##### Current track information
-    ##track = spotifyObject.current_user_playing_track()
-    ##artist = track['item']['artists'][0]['name']
-    ##track = track['item']['name']
-    ##
-    ##if artist != "":
-    ##    print("Currently playing " + artist + " - " + track)
-
-    # User information
-    user = sp.current_user()
-    displayName = user['display_name']
-    followers = user['followers']['total']
-    #print(user)
-    #print(dir(user))
-
-    #result = sp.search('Mogwai')
-    #print(result)
-
-    track = sp.current_user_playing_track()
-    #print(list(track))
+    # NOTE: While the current_user_playing_track() attribute results in a dictionary, the album art image URL is not assigned to a key and must be extracted by parsing.
     items = track['item']
+    
+    # Convert dictionary object to string and parse the album art image URL.
     string_items = str(items)
     image_loc = string_items.find('images')
     image_loc_end = string_items.find('width', image_loc)
     image_url = string_items[image_loc+34:image_loc_end-4]
+
+    # Use the urllib library to save the image as a temporary file in a sub-directory.
     req.urlretrieve(image_url, "Temporary Image Directory/temp_artwork.jpg")
-    #print(test_item)
-    #print(dir(track))
-    #image_url = track['is playing']
-    #print(image_url)
+    #print(image_url) # For Debug
 
     from PIL import Image, ImageFilter
 #    try:
