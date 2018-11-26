f7c73071d88f2b92f8da40150e7ace5e6d5022f4
Chris Penny <chrispenny11@gmail.com>
2018-11-26 17:14:43 -0500

    implemented second color selection loop, added comments

--- Spotify and Hue/Color_Extraction_Main.py
+++ Spotify and Hue/Color_Extraction_Main.py
@@ -51,15 +51,15 @@ def get_artwork_colors():
 
     album_art_test = Image.open("Temporary Image Directory/temp_artwork.jpg").convert("L")
     album_art = Image.open("Temporary Image Directory/temp_artwork.jpg")
-    print(album_art.mode)
+#    print(album_art.mode) # For Debug
 
     histogram = album_art.histogram()
-#    print(rgb_data)
+#    print(rgb_data) # For Debug
 
 # Convert image to a numpy array.
     rgb_tuples = np.asarray(album_art)
-#    print(rgb_tuples)
-#    print(type(rgb_tuples.shape))
+#    print(rgb_tuples) # For Debug
+#    print(type(rgb_tuples.shape)) # For Debug
 
 
 # Stack with Numpy
@@ -69,28 +69,23 @@ def get_artwork_colors():
         if i > 0:
             np_stack = np.vstack((np_stack, rgb_tuples[i]))
 
-    print(np.shape(np_stack))
+#    print(np.shape(np_stack)) # For Debug
 
     pd_rgb_stack = pd.DataFrame(np_stack, columns = ['r', 'g', 'b'])
-#    print(pd_rgb_stack['r'])
     pd_rgb_stack['rgb'] = (pd_rgb_stack['r'].astype(str)) + '_' + (pd_rgb_stack['g'].astype(str)) + '_' + (pd_rgb_stack['b'].astype(str))
     pd_rgb_stack['check_sum'] = pd_rgb_stack[['r', 'b', 'g']].sum(axis=1)
+#    print(pd_rgb_stack['check_sum'].head(20)) # For Debug
+    check_sum_mean = pd_rgb_stack['check_sum'].mean()
+#    print(check_sum_mean) # For Debug
+
 
 #   Sort by Value Counts to find modal values and return in order of frequency
     pd_rgb_sorted = pd_rgb_stack['rgb'].value_counts().to_frame()
     pd_rgb_sorted.astype(int)
-    print(pd_rgb_sorted.iloc[0])
-    print(pd_rgb_sorted)
-    print(pd_rgb_stack['rgb'].head())
-    print(pd_rgb_stack['check_sum'].head(20))
-    check_sum_mean = pd_rgb_stack['check_sum'].mean()
-    print(check_sum_mean)
-#    r_select = pd_rgb_sorted['r'].values[0]
-#    r_select = pd_rgb_sorted.loc[1, 'r']
-    print(pd_rgb_stack['r'])
-#    print(r_select)
-
-#Guiding Idea: Take the highest frequency rgb combination that is a sufficient distance from the max value.
+#    print(pd_rgb_sorted.iloc[0]) # For Debug
+#    print(pd_rgb_sorted) # For Debug
+#    print(pd_rgb_stack['rgb'].head()) # For Debug
+#    print(pd_rgb_stack['r']) # For Debug
 
 # Initialize Values for While Loop
     rgb_temp_std = 0
@@ -98,9 +93,9 @@ def get_artwork_colors():
     i = -1
 
     #and (rgb_temp_sum < 50)
-# Implement While Loop to Ensure Color is not black/white/greyscale (in which all rgb values are approximately the same)
+# Implement While Loop to ensure selected color is not black/white/greyscale (in which all rgb values are approximately the same)
     while (rgb_temp_std < 10):
-        i = i + 1
+        i += 1
         temp = pd_rgb_sorted.index[i]
         parse_1 = temp.find('_')
         parse_2 = temp.find('_', parse_1+1)
@@ -108,41 +103,54 @@ def get_artwork_colors():
         rgb_temp_std = np.std(rgb_temp, 1)[0]
         rgb_temp_sum = rgb_temp['r'] + rgb_temp['g'] + rgb_temp['b']
 
-    rgb_select = rgb_temp
-    print(rgb_temp)
+    rgb_select_1 = rgb_temp
+    print('The first selected color is:')
+    print(rgb_select_1)
 
-    pd_rgb_stack['r_select'] = rgb_select['r'][0]
-    pd_rgb_stack['g_select'] = rgb_select['g'][0]
-    pd_rgb_stack['b_select'] = rgb_select['b'][0]
+# Write selected rgb values to respective variables for use in distance calculation
+    pd_rgb_stack['r_select'] = rgb_select_1['r'][0]
+    pd_rgb_stack['g_select'] = rgb_select_1['g'][0]
+    pd_rgb_stack['b_select'] = rgb_select_1['b'][0]
 
-#    pd_rgb_stack['dist_score'] = pd_rgb_stack['r'] - r_select
+# Perform distance calculation (<-- distance formula applied to RGB values)
     pd_rgb_stack['dist_score'] = np.sqrt(np.square(pd_rgb_stack['r'] - pd_rgb_stack['r_select']) + np.square(pd_rgb_stack['g'] - pd_rgb_stack['g_select']) + np.square(pd_rgb_stack['b'] - pd_rgb_stack['b_select']))
     print(pd_rgb_stack['dist_score'].head(20))
+# Inspect Counts
     dist_score_sorted = pd_rgb_stack['dist_score'].value_counts().to_frame()
+# Calculate Standard Deviation of Distance Score
     dist_score_std = np.std(pd_rgb_stack['dist_score'])
     print(dist_score_std)
 
-# Recreate Sorted List On DF including distance score -- Merge/Join?
-    pd_rgb_sorted = pd_rgb_stack['rgb'].value_counts().to_frame()
-    pd_rgb_sorted.astype(int)
-
-# Reset Initial Values for Second While Loop
-    rgb_temp_std = 0
-    rgb_temp_sum = 0
-    j = -1
+# Recreate Sorted List on DF
+##    pd_rgb_sorted = pd_rgb_stack['rgb'].value_counts().to_frame()
+##    pd_rgb_sorted.astype(int)
 
 # Perform left join on pd_rgb_stack to get distance scores in same dataframe
     pd_rgb_merged = pd_rgb_stack.join(pd_rgb_sorted, on = 'rgb', how = 'left', rsuffix = '_count')
 
+# Sort Merged DF
+    pd_rgb_merged = pd_rgb_merged.sort_values(by = ['rgb_count', 'dist_score'], ascending = False)
+
+# Reset Initial Values for Second While Loop
+    rgb_temp_std = 0
+    rgb_temp_sum = 0
+    temp_dist_score = 0
+    j = -1
+
 # Second While Loop    
-##    while (rgb_temp_std < 10):
-##        j = j + 1
-##        temp = pd_rgb_sorted.index[j]
-##        parse_1 = temp.find('_')
-##        parse_2 = temp.find('_', parse_1+1)
-##        rgb_temp = pd.DataFrame({'r':[temp[0:parse_1]], 'g':[temp[parse_1+1: parse_2]], 'b':[temp[parse_2+1:]]}).astype(int)
-##        rgb_temp_std = np.std(rgb_temp, 1)[0]
-##        rgb_temp_sum = rgb_temp['r'] + rgb_temp['g'] + rgb_temp['b']
+    while (rgb_temp_std < 10 or temp_dist_score < dist_score_std):
+        j += 1
+        temp = pd_rgb_merged.iloc[j]['rgb']
+        temp_dist_score = pd_rgb_merged.iloc[j]['dist_score']
+        parse_1 = temp.find('_')
+        parse_2 = temp.find('_', parse_1+1)
+        rgb_temp = pd.DataFrame({'r':[temp[0:parse_1]], 'g':[temp[parse_1+1: parse_2]], 'b':[temp[parse_2+1:]]}).astype(int)
+        rgb_temp_std = np.std(rgb_temp, 1)[0]
+        rgb_temp_sum = rgb_temp['r'] + rgb_temp['g'] + rgb_temp['b']
+
+    rgb_select_2 = rgb_temp
+    print('The second selected color is:')
+    print(rgb_select_2)
 
 # Generate Histograms and Plot
             
